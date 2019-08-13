c60f3c4f17138422b658911ad6fff8d39bd0ba4a
javathehutt23 <harryfurnish@gmail.com>
2019-08-13 13:47:37 +1200

    fixed the topup page balance functional
    topup function works now

--- WebsiteFiles/scripts/topup.js
+++ WebsiteFiles/scripts/topup.js
@@ -1,16 +1,25 @@
+const users = new User();
 const session = new Session();
 var currentUser = session.get('currentUser');
-console.log(session.get('currentUser'))
+
+console.log(session.get('currentUser'));
+
 function topupButton(){
   var amount = +document.getElementById("topupAmount").value;
-  if (amount > 0){
-    currentUser.balance += amount;
-    var ssss = currentUser.balance += amount;
-    alert(ssss);
-    session.set('currentUser');
+
+  if (amount > 0)
+  {
+    currentUser.Balance += amount;
+    var ssss = currentUser.Balance;
+    console.log(ssss);
+    var current = session.set('currentUser', currentUser);
+    users.updateUser(current, 'currentUser');
+    console.log(session.get('currentUser'));
   }
-  else{
-    alert("you need to input a valid number larger than 0")
+
+  else
+  {
+    alert("you need to input a valid number larger than 0");
   }
-  console.log(session.get('currentUser'))
+
 }
